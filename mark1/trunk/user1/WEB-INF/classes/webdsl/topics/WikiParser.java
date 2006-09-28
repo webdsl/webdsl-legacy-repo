b5fa110ef41b951dbaf5dfecaccc5492ac5c9a62
Eelco Visser <eelcovis@gmail.com>
2006-09-28 21:26:19 +0000

    - forced links - wiki words - verbatim sections
    svn path=/; revision=60

--- mark1/trunk/user1/WEB-INF/classes/webdsl/topics/WikiParser.java
+++ mark1/trunk/user1/WEB-INF/classes/webdsl/topics/WikiParser.java
@@ -1,6 +1,7 @@
 package webdsl.topics;
 
 import java.io.*;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -20,6 +21,7 @@ public class WikiParser
     private String at_end_of_line = "";
     private String at_end_of_par = "";
     private int list_level = 0;
+    private boolean eof = false;
 
     public WikiParser(Writer out, String doc)
     {
@@ -30,7 +32,7 @@ public class WikiParser
 	this.line  = 0;
     }
 
-    public boolean nextChar()
+    private boolean nextChar()
     {
 	if (pos < len)
 	    {
@@ -39,24 +41,78 @@ public class WikiParser
 		return true;
 	    }
 	else
-	    return false;
+	    {
+		eof = true;
+		return false;
+	    }
+    }
+
+    private void resetCurrentChar(int pos)
+    {
+	if (pos > 1)
+	    {
+		this.pos = pos;
+		curchar = text.charAt(pos - 1);
+	    }
+	else
+	    {
+		this.pos = 0;
+		nextChar();
+	    }
     }
 
     public void parse() throws IOException
     {
 	out.write("this text rendered by WikiParser <p />\n");
 	nextChar();
-	if (startNewLine()) nextChar();
+	if (startNewLine()) 
+	    nextChar();
 	do {
 	    switch (curchar) {
 	    case '\n' :
 		startNewLine();
 		break;
 
-		//case '[' :
-		//parseLink();
-		//break;
-		
+	    case '[' :
+		if (!parseLink())
+		    out.write(curchar);
+		break;
+
+	    case '/' :
+		if(!matchPathExp())
+		    out.write(curchar);
+		break;
+
+	    case 'A' :
+	    case 'B' :
+	    case 'C' :
+	    case 'D' :
+	    case 'E' :
+	    case 'F' :
+	    case 'G' :
+	    case 'H' :
+	    case 'I' :
+	    case 'J' :
+	    case 'K' :
+	    case 'L' :
+	    case 'M' :
+	    case 'N' :
+	    case 'O' :
+	    case 'P' :
+	    case 'Q' :
+	    case 'R' :
+	    case 'S' :
+	    case 'T' :
+	    case 'U' :
+	    case 'V' :
+	    case 'W' :
+	    case 'X' :
+	    case 'Y' :
+	    case 'Z' :
+		if(!matchWikiWord())
+		    out.write(curchar);
+		break;
+
 	    default :
 		out.write(curchar);
 	    }
@@ -64,7 +120,7 @@ public class WikiParser
 	out.write("<p /> full text rendered by WikiParser <p />\n");
     }
 
-    public boolean startNewLine() throws IOException
+    private boolean startNewLine() throws IOException
     {
 	line++;
 	out.write(at_end_of_line);
@@ -72,15 +128,16 @@ public class WikiParser
 	return newParagraph() 
 	    || sectionHeading() 
 	    || listItem()
-	    || separator();
+	    || separator()
+	    || verbatim();
     }
 
-    public void skipWhiteSpace() throws IOException
+    private void skipWhiteSpace() throws IOException
     {
 	while(curchar == ' ' || curchar == '\t') { nextChar(); }
     }
 
-    public int readChars(char c)
+    private int readChars(char c)
     {
 	int count = 0;
 	while(curchar == c)
@@ -91,7 +148,7 @@ public class WikiParser
 	return count;
     }
 
-    public boolean newParagraph() throws IOException
+    private boolean newParagraph() throws IOException
     {
 	// ([\ \t]* [\n])+	
 	if(skipBlankLine())
@@ -113,7 +170,7 @@ public class WikiParser
 	    return false;
     }
 
-    public boolean skipBlankLine() throws IOException
+    private boolean skipBlankLine() throws IOException
     {
 	// [\ \t]* [\n]
 	int oldpos = pos;
@@ -125,12 +182,36 @@ public class WikiParser
 	    }
 	else
 	    {
-		pos = oldpos;
+		resetCurrentChar(oldpos);
 		return false;
 	    }
     }
 
-    public boolean sectionHeading() throws IOException
+    private void copyLineVerbatim() throws IOException
+    {
+	// ~[\n]* [\n]
+	nextChar();
+	while (curchar != '\n' || curchar == '\r')
+	    {
+		switch (curchar) {
+		case '<' : 
+		    out.write("&lt;");
+		    break;
+		case '>' :
+		    out.write("&gt;");
+		    break;
+		case '&' :
+		    out.write("&amp;");
+		    break;
+		default : 
+		    out.write(curchar);
+		    break;
+		}
+		nextChar();
+	    }
+    }
+
+    private boolean sectionHeading() throws IOException
     {
 	// \n ([-]3+ [\+]+ [\n])+
 	
@@ -147,12 +228,12 @@ public class WikiParser
 	    }
 	else
 	    {
-		pos = oldpos;
+		resetCurrentChar(oldpos);
 		return false;
 	    }
     }
 
-    public boolean listItem() throws IOException
+    private boolean listItem() throws IOException
     {
 	int oldpos = pos;
 	nextChar();
@@ -176,12 +257,12 @@ public class WikiParser
 	    }
 	else
 	    {
-		pos = oldpos;
+		resetCurrentChar(oldpos);
 		return false;
 	    }
     }
 
-    public boolean separator() throws IOException
+    private boolean separator() throws IOException
     {
 	// [-]3+ 
 	
@@ -196,18 +277,242 @@ public class WikiParser
 	    }
 	else
 	    {
-		pos = oldpos;
+		resetCurrentChar(oldpos);
 		return false;
 	    }
     }
 
-    public void parseLink()
+    private boolean parseLink() throws IOException
     {
+	int oldpos = pos;
+	String link = bracketed();
+	if (link != null)
+	    {
+		String ref = bracketed();
+		if (nextChar() && curchar == ']')
+		    {
+			if (ref == null)
+			    ref = link;
+			// todo: process wikiwords
+			out.write("<a href=\"" + wikiWordToURL(link) + "\">" + ref + "</a>");
+			return true;
+		    }
+	    }
+	resetCurrentChar(oldpos);
+	return false;
     }
 
-    public boolean verbatim()
+    private boolean matchWikiWord() throws IOException
     {
+	
+	if (matchQualifiedWikiWord() || matchPathExp())
+	    return true;
+
+	String word = matchBasicWikiWord();
+	if (word != null)
+	    {
+		out.write("<a href=\"" + word + "\">" + word + "</a>");	
+		return true;
+	    }
 	return false;
+    }
+
+    private boolean matchQualifiedWikiWord() throws IOException
+    {
+	int oldpos = pos;
+
+	StringBuffer pref = new StringBuffer("");
+
+	if(isUpper(curchar))
+	    {
+		pref.append(curchar);
+		while (nextChar() && (isUpper(curchar) || isLower(curchar)))
+		    {
+			pref.append(curchar);
+		    }
+		
+		if (curchar == '.' && nextChar() && isUpper(curchar))
+		    {
+			String suff = matchBasicWikiWord();
+			if (suff != null)
+			    {
+				out.write("<a href=\"/user1/view/" + pref + "/" + suff + "\">" + suff + "</a>" );
+				return true;
+			    }
+		    }
+	    }
+	resetCurrentChar(oldpos);
+	return false;
+    }
+
+    private String matchBasicWikiWord() throws IOException
+    {
+	int oldpos = pos;
+	StringBuffer word = new StringBuffer("");
+	if (isUpper(curchar))
+	    {
+		word.append(curchar);
+		while (nextChar() && isLower(curchar))
+		    word.append(curchar);
+		if (isUpper(curchar))
+		    {
+			word.append(curchar);
+			while (nextChar() && (isUpper(curchar) || isLower(curchar)))
+			    {
+				word.append(curchar);
+			    }
+			resetCurrentChar(pos - 1);
+			return word.toString();
+		    }
+	    }
+	resetCurrentChar(oldpos);
+	return null;
+    }
+    
+    private boolean matchPathExp() throws IOException
+    {
+	int oldpos = pos;
+	boolean hasslash = false;
+	StringBuffer path = new StringBuffer("");
+
+	path.append(curchar);
+	hasslash = hasslash || curchar == '/';
+	while (nextChar() 
+	       && (isUpper(curchar) || isLower(curchar) 
+		   || isDigit(curchar) || curchar == '/'))
+	    {
+		hasslash = hasslash || curchar == '/';
+		path.append(curchar);
+	    }
+	if (hasslash)
+	    {
+		if (path.charAt(0) == '/')
+		    out.write("<a href=\"/user1/view" + path + "\">" + path + "</a>");
+		else
+		    out.write("<a href=\"" + path + "\">" + path + "</a>");
+		return true;
+	    }
+	resetCurrentChar(oldpos);
+	return false;
+    }
+
+    private boolean isUpper(char c)
+    {
+	return (int)c >= 65 && (int)c <= 90;
+    }
+
+    private boolean isLower(char c)
+    {
+	return (int)c >= 97 && (int)c <= 122;
+    }
+
+    private boolean isDigit(char c)
+    {
+	return (int)c >= 48 && (int)c <= 57;
+    }
+
+    private String wikiWordToURL(String s)
+    {
+	if (s.startsWith("http:") || s.startsWith("ftp:"))
+	    {
+		// an absolute URL
+		return s;
+	    }
+	else if (s.startsWith("/"))
+	    {
+		// an 'absolute' wiki path
+		return "/user1/view" + s;
+	    }
+	else if (s.matches("[A-Z][a-z]+[A-Z][a-z]+"))
+	    {
+		// wiki word relative to current 'directory'
+		return s;
+	    }
+	else if (s.matches("[A-Z][A-Za-z]*\\.[A-Z][a-z]+[A-Z][a-z]+"))
+	    {
+		return "/user1/view/" + s.replaceFirst("\\.", "/");
+	    }
+	else 
+	    { 
+		// interpret as phrase to be wikiwordified
+		// capitalize first letters of each word
+		// e.g. [[wiki word]] -> [[WikiWord]]
+
+		StringBuffer b = new StringBuffer(s);
+		StringBuffer c = new StringBuffer("");
+		
+		c.append(b.substring(0,1).toUpperCase());
+		for(int i = 1; i < b.length(); i++)
+		    {
+			if (b.charAt(i) == ' ')
+			    {
+				if(i + 1 < b.length())
+				    {
+					i++;
+					c.append(b.substring(i,i+1).toUpperCase());
+				    }
+			    }
+			else
+			    {
+				c.append(b.charAt(i));
+			    }
+		    }
+		return c.toString();
+	    }
+    }
+
+    private String bracketed()
+    {
+	int oldpos = pos;
+	StringBuffer b = new StringBuffer("");
+
+	if (nextChar() && curchar == '[')
+	    {
+		while(nextChar() && curchar != ']' 
+		      && curchar != '\n' && curchar != '\r')
+		    {
+			b.append(curchar);
+		    }
+		if (curchar == ']')
+		    return b.toString();
+	    }
+	resetCurrentChar(oldpos);
+	return null;
+    }
+
+    private boolean verbatim() throws IOException
+    {
+	if (!matchWord("<verbatim>"))
+	    {
+		return false;
+	    }
+	
+	out.write("<pre>\n");
+
+	while (!matchWord("</verbatim>") && !eof)
+	    copyLineVerbatim();
+
+	out.write("</pre>\n");
+
+	return true;
+    }
+    
+    private boolean matchWord(String w) throws IOException
+    {
+	int wlen = w.length();
+	int oldpos = pos;
+	boolean match = true;
+	for (int i = 0; i < wlen && nextChar(); i++)
+	    {
+		if (w.charAt(i) != curchar)
+		    {
+			match = false;
+			break;
+		    }
+	    }
+	if (!match)
+	    resetCurrentChar(oldpos);
+	return match;
     }
 
 }
