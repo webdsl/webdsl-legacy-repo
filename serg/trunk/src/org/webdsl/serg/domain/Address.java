fe78d7024e128601800c9bad7c37552a794ec8cd
Eelco Visser <eelcovis@gmail.com>
2007-04-11 11:56:11 +0000

    Address didn't have getter and setter for id (should have been generated)
    svn path=/; revision=175

--- serg/trunk/src/org/webdsl/serg/domain/Address.java
+++ serg/trunk/src/org/webdsl/serg/domain/Address.java
@@ -9,7 +9,17 @@ import javax.persistence.*;
   { }
 
   @Id @GeneratedValue private Long id;
+  
+  public Long getId()
+  {
+	  return id;
+  }
 
+  private void setId(Long id)
+  {
+     this.id = id;
+  }
+  
   private String street;
 
   public String getStreet()
