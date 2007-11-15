0b1fb2c8c486d96c067d1ae118e8ad57bb4853f5
Lennart Kats <lclkats@gmail.com>
2007-11-15 17:00:19 +0000

    * Specific parse controller instances should provide their own ast node factory.   For now I just provide the default factory.
    * Restored token highlighter. Doesn't work yet until asfix tokenization is complete.
    
    svn path=/; revision=560

---
+++ imp/trunk/org.webdsl.ide.imp/src/webdsl/safari/parser/WebDSLParsersym.java
@@ -0,0 +1,14 @@
+package webdsl.safari.parser;
+
+import org.strategoxt.imp.runtime.parser.ast.SGLRParsersym;
+
+/**
+ * Interface containing constant values for the WebDSL parser token kinds.
+ * 
+ * @note This file should be generated.
+ * 
+ * @author Lennart Kats <L.C.L.Kats add tudelft.nl>
+ */
+public interface WebDSLParsersym extends SGLRParsersym {
+
+}
