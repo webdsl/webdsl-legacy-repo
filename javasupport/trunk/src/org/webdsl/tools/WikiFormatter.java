5f3e6cb39a900ff35f0d7cb1bcf3bef9a2a7c1f3
Zef Hemel <zef@zef.me>
2007-11-20 14:53:19 +0000

    Implemented 0 or 1 arguments wiki parser. Syntax examples:
    [[home]]
    [[home()]]
    [[page(MainPage)]]
    [[home|go to homepage]]
    [[page(MainPage)|the main page]]
    
    svn path=/; revision=584

--- javasupport/trunk/src/org/webdsl/tools/WikiFormatter.java
+++ javasupport/trunk/src/org/webdsl/tools/WikiFormatter.java
@@ -4,13 +4,24 @@ import com.petebevin.markdown.*;
 import java.util.regex.*;
 
 public final class WikiFormatter {
-	public static String wikiFormat(String text) {
-		Pattern links = Pattern.compile("\\[\\[(\\w+)\\(([^\\)]+)\\)\\]\\]");
+	private static final Pattern links = Pattern.compile("\\[\\[(\\w+)(\\(([^\\)]*)\\))?(\\|([^\\]]+))?\\]\\]");
+
+	public static String wikiFormat(String text, String rootUrl) {
 		Matcher m = links.matcher(text);
 		StringBuffer sb = new StringBuffer();
 		while (m.find()) {
-			System.out.println("Found a link!");
-			m.appendReplacement(sb, "["+m.group(2)+"](" + m.group(1) + ".seam?" + m.group(1) + "=" + m.group(2) + ")");
+			String url, title;
+			if(m.group(2) == null || m.group(3).length() == 0) {
+				url = rootUrl + "/" + m.group(1);
+				title = m.group(1);
+			} else {
+				url = rootUrl + "/" + m.group(1) + "/" + m.group(3);
+				title = m.group(3);
+			}
+			if(m.group(5) != null) {
+				title = m.group(5);
+			}
+			m.appendReplacement(sb, "[" + title + "](" + url + ")");
 		}
 		m.appendTail(sb);
 		MarkdownProcessor processor = new MarkdownProcessor();
@@ -20,6 +31,6 @@ public final class WikiFormatter {
 	public static void main(String[] args) {
 		System.out.println("The output:");
 		System.out
-				.println(wikiFormat("This is a header\n=========\n\nAnd here's a link [[page(MainPage)]].\nHello people!"));
+				.println(wikiFormat("This is a header\n=========\n\nAnd here's a link [[page(MainPage)|Main page]].\nHello people! [[home()]] -- [[home|Terug naar de homepage]]", "/test"));
 	}
 }
