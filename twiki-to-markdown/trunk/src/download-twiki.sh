83f7789acec2acb20712bc96f3416c96c11650bd
Eelco Visser <eelcovis@gmail.com>
2008-01-02 00:47:25 +0000

    improving twiki to markdown conversion
    svn path=/; revision=862

--- twiki-to-markdown/trunk/src/download-twiki.sh
+++ twiki-to-markdown/trunk/src/download-twiki.sh
@@ -22,15 +22,18 @@ do
 
       sed s/\'/\\\'/g  ${web}/${topic}.md > ${web}/${topic}.md.esc 
 
-#      cp ${web}/${topic}.md ${web}/${topic}.md.esc 
+#     cp ${web}/${topic}.md ${web}/${topic}.md.esc 
 
       #echo curl -F "j_id86:j_id91:j_id92=${topic}" \
       #	   -F "j_id86:j_id98:j_id99=<${web}/${topic}.md" \
       #	   http://localhost:8080/strategoxt/newPage.seam
 
-      echo "insert into TwikiPage (_key, _content) " \
-           " values ('${topic}', '`cat ${web}/${topic}.md`');" \
-	   >> inserts.msql
+      
+      echo "insert into TwikiPage (_key, _web, _topic, _content) " \
+           " values ('${web}/${topic}', '${web}', '${topic}', '" >> inserts.msql
+      cat ${web}/${topic}.md >> inserts.msql
+      echo "');" >> inserts.msql
+
     done
   fi
 done
