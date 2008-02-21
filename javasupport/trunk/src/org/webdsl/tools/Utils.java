d7b52f01f3b5208a7f5846377410c6c9a6559a66
Zef Hemel <zef@zef.me>
2008-02-21 15:21:06 +0000

    Added isInstance and WebDSLEntity interface
    svn path=/; revision=985

--- javasupport/trunk/src/org/webdsl/tools/Utils.java
+++ javasupport/trunk/src/org/webdsl/tools/Utils.java
@@ -8,55 +8,65 @@ import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-public final class Utils {
-	public static Date parseDate(String date, String format) {
-		try {
-			return new SimpleDateFormat(format).parse(date);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	/*public static boolean equal(int a, int b) {
-		return a == b;
-	}
-	
-	public static boolean equal(double a, double b) {
-		return a == b;
-	}
-	
-	public static boolean equal(int a, Integer b) {
-		return b.equals(a);
-	}
+import org.webdsl.WebDSLEntity;
 
-	public static boolean equal(double a, Double b) {
-		return b.equals(a);
-	}*/
-	
-	public static boolean equal(Object a, Object b) {
-		if(a == null && b == null) {
-			return true;
-		} else if(a == null || b == null) {
-			return false;
-		}
-		return a.equals(b);
-	}
-	
-	public static void download(FacesContext facesContext, byte[] file, String name, String type) {
-		if (!facesContext.getResponseComplete()) {
-	    	   HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
-	    	   response.setContentType(type);
-	    	   response.setContentLength(file.length);
-	    	   response.setHeader("Content-disposition","attachment; filename=" + name);
-	    	   ServletOutputStream out;
-	    	   try {
-	    		   out = response.getOutputStream();
-	    		   out.write(file);
-	    		   out.flush();
-	    	   } catch (IOException e) {
-	    		//TODO: something  
-	    	   }
-	    	   facesContext.responseComplete();
-	       }
-	}
+public final class Utils {
+    public static Date parseDate(String date, String format) {
+        try {
+            return new SimpleDateFormat(format).parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*public static boolean equal(int a, int b) {
+        return a == b;
+    }
+
+    public static boolean equal(double a, double b) {
+        return a == b;
+    }
+
+    public static boolean equal(int a, Integer b) {
+        return b.equals(a);
+    }
+
+    public static boolean equal(double a, Double b) {
+        return b.equals(a);
+    }*/
+
+    public static boolean equal(Object a, Object b) {
+        if(a == null && b == null) {
+            return true;
+        } else if(a == null || b == null) {
+            return false;
+        }
+        return a.equals(b);
+    }
+
+    public static boolean isInstance(Object o, Class<?> c) {
+        if(o instanceof WebDSLEntity) {
+            return ((WebDSLEntity)o).isInstance(c);
+        } else {
+            return c.isInstance(o);
+        }
+    }
+
+    public static void download(FacesContext facesContext, byte[] file, String name, String type) {
+        if (!facesContext.getResponseComplete()) {
+            HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+            response.setContentType(type);
+            response.setContentLength(file.length);
+            response.setHeader("Content-disposition","attachment; filename=" + name);
+            ServletOutputStream out;
+            try {
+                out = response.getOutputStream();
+                out.write(file);
+                out.flush();
+            } catch (IOException e) {
+                //TODO: something
+            }
+            facesContext.responseComplete();
+        }
+    }
 }
