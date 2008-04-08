b5ee282e64d12fbc953be52800a657fe704be223
Sander Vermolen <sandervermolen@gmail.com>
2008-04-08 12:48:52 +0000

    bugfix
    svn path=/; revision=1092

--- webdslMigration/migration/transformation/EmptyObject.java
+++ webdslMigration/migration/transformation/EmptyObject.java
@@ -9,6 +9,8 @@ public class EmptyObject extends UntypedTransformation {
 
 	@Override
 	public Object getAttribute(List<UntypedTransformation> input, TransformationScope scope, String attributeName) throws TransformationException {
+		if(attributeName.equals("id"))
+			return new Long(0);	// TODO Added this for WebDSL compatibility (maybe fix in migration mapping step)
 		throw new TransformationException("Attribute "+attributeName+" is not part of the transformation (getAttribute arrived at EmptyObject)");
 	}
 
