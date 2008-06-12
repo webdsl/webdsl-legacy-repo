1c3627ea0626d444d9229a687f18a166609688fe
Zef Hemel <zef@zef.me>
2008-06-12 12:02:54 +0000

    Update
    svn path=/; revision=1358

--- test_apps/test-typechecker/run-test.sh
+++ test_apps/test-typechecker/run-test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 name=$0.app
-dsl-to-seam -i $name --stop-after 1 > /dev/null 2> .$name.out
-test 0 -ne $?
-#if test 0 -eq $?; then
-#  echo "$name did not return any error but should"
-#else
-#  echo "$name succeeded!"
-#fi
+show_result=$1
+dsl-to-seam -i $name --stop-after 1 > /dev/null 2> $name.out
+result=$?
+if test 0 -ne $result; then
+  echo "PASS: $name"
+  rm $name.out
+else
+  if [ "$show_result" == "show" ]; then
+    echo "FAIL: $name"
+    cat $name.out
+  else
+    echo "FAIL: $name (output in $name.out)"
+  fi
+fi
+test 0 -ne $result
