d7b2d052c43fb4c794e95af069a2747da1222039
Sander van der Burg <svanderburg@gmail.com>
2009-08-12 14:57:35 +0000

    Removed ATerm library
    svn path=/; revision=2902

--- webdsldeploy/trunk/libraries/aterm/default.nix
+++
@@ -1,9 +0,0 @@
-{stdenv, fetchurl}:
-
-stdenv.mkDerivation {
-  name = "aterm-2.5";
-  src = fetchurl {
-    url = ftp://ftp.stratego-language.org/pub/stratego/StrategoXT/strategoxt-0.17/aterm-2.5.tar.gz;
-    sha256 = "164di6my7iy2ybfl46fa2hw6fn6vgqfn2jrpy5mkpcbb4cc5pg4v";
-  };
-}
