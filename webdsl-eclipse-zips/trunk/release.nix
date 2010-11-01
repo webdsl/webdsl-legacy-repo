8c1dc9257cc5cbd1f54df30723a5cae96c4182e2
Rob Vermaas <rob.vermaas@gmail.com>
2010-11-01 08:33:59 +0000

    webdsl-eclipse-zips: add hashes for eclipse downloads, allow network access
    svn path=/; revision=4239

--- webdsl-eclipse-zips/trunk/release.nix
+++ webdsl-eclipse-zips/trunk/release.nix
@@ -3,27 +3,27 @@ let
 
   eclipseWin = pkgs.fetchurl {
       url = http://download.springsource.com/release/ECLIPSE/helios/SR1/eclipse-SDK-3.6.1-win32.zip ;
-      sha256 = ""; 
+      sha256 = "08chhq5y9z3v1ld0p2mf1xakhj4vs9s25ayvcn9h748mzhaahzda"; 
     };
 
   eclipseLinux = pkgs.fetchurl {
       url = http://download.springsource.com/release/ECLIPSE/helios/SR1/eclipse-SDK-3.6.1-linux-gtk.tar.gz ;
-      sha256 = ""; 
+      sha256 = "0s48rjaswi8m5gan1zlqvfwb4l06x5nslkq41wpkrbyj9ka8gh4x"; 
     };
 
   eclipseLinux64 = pkgs.fetchurl {
       url = http://download.springsource.com/release/ECLIPSE/helios/SR1/eclipse-SDK-3.6.1-linux-gtk-x86_64.tar.gz ;
-      sha256 = ""; 
+      sha256 = "1cg9rrb5w978sdqbzz9lnli1lds9zhb6wfsj3wp725bqf1i6v9lg"; 
     };
 
   eclipseMac = pkgs.fetchurl {
       url = http://download.springsource.com/release/ECLIPSE/helios/SR1/eclipse-SDK-3.6.1-macosx-cocoa.tar.gz ;
-      sha256 = ""; 
+      sha256 = "1jdsnipbf4db0i0g9vcpnmh4mdgg874p15677wibm4righ9cj9n3"; 
     };
 
   eclipseMac64 = pkgs.fetchurl {
       url = http://download.springsource.com/release/ECLIPSE/helios/SR1/eclipse-SDK-3.6.1-macosx-cocoa-x86_64.tar.gz ;
-      sha256 = ""; 
+      sha256 = "0b3pa4vc68dcxc9db7gj1vcikinxppxs5bi3rydsxzzsycrm0vly"; 
     };
 
   eclipseZip = { name, eclipse, system }:
@@ -31,6 +31,7 @@ let
         eclipseini = (if pkgs.stdenv.isDarwin then "Eclipse.app/Contents/MacOS/" else "" ) + "eclipse.ini";
     in
     pkgs.stdenv.mkDerivation rec {
+      __noChroot = true;
 
       inherit name;
       buildInputs = [pkgs.jdk pkgs.zip pkgs.unzip pkgs.perl]; 
@@ -46,7 +47,6 @@ let
         ALL_SITES="$WEBDSL_SITE,$SPOOFAX_SITE,$EMF_SITE,$WTP_SITE"
 
         echo "Copying eclipse..."
-
         mkdir data 
         ${if system == "i686-cygwin" then "unzip ${eclipse}" else "tar xzf ${eclipse}"}
         cd eclipse
@@ -59,6 +59,7 @@ let
         sed -i 's|-XX:MaxPermSize=[0-9]*m||' ${eclipseini}
         sed -i '/^$/d' ${eclipseini}
         perl -pi -e "s/^\r\n//" ${eclipseini}
+
 
         # add own default vmwargs
         echo "-Xms256m" >> ${eclipseini}
@@ -96,6 +97,5 @@ in
       ln -s ${ eclipseZip { name = "eclipsemac64.zip";   eclipse = eclipseMac64;   system = "x86_64-darwin"; } } $out/eclipsemac64.zip
       ln -s ${ eclipseZip { name = "eclipsemac.zip";     eclipse = eclipseMac;     system = "i686-darwin";   } } $out/eclipsemac.zip
     '';
-
 
 }
