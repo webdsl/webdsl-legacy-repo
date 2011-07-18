c864409e68e1848baeb1317765299486a85205f5
Sander van der Burg <svanderburg@gmail.com>
2011-07-18 11:59:08 +0000

    Replaced reference to my homedir with a relative path
    svn path=/; revision=4766

--- webdsldeploy/trunk/development/webdsl/default.nix
+++ webdsldeploy/trunk/development/webdsl/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation {
   name = "webdsl-trunk";
-  src = /home/sander/webdsls;
+  src = ../../../webdsls;
   configureFlags = "--enable-web-check=no";
   preConfigure = "ulimit -s ${if stdenv.isDarwin then "64000" else "unlimited"}; ./bootstrap";
   buildInputs = [ aterm sdf strategoxt javafront perl autoconf automake libtool pkgconfig apacheAnt ];
