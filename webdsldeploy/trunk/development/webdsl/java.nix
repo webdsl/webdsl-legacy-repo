1ed1b172e424d87aad5ab00814ec3e2c965a7875
Danny Groenewegen <dgroenewegen@gmail.com>
2012-07-04 10:57:06 +0000

    use newer webdsl version
    svn path=/; revision=5227

--- webdsldeploy/trunk/development/webdsl/java.nix
+++ webdsldeploy/trunk/development/webdsl/java.nix
@@ -3,8 +3,8 @@
 stdenv.mkDerivation {
   name = "webdsl-java";
   src = fetchurl {
-    url = http://hydra.nixos.org/build/2613427/download/1/webdsl-java.zip;
-    sha256 = "0f6yka3j0v8bsmg6zfhs7kvfg9w6jykjkxl4pipld0mhmjc6hh35";
+    url = http://hydra.nixos.org/build/2740829/download/1/webdsl-java.zip;
+    sha256 = "d430eb5803bd08a63026e67d5b4314f8f34f00299c7b0346a44f07eff2a616fa";
   };
   buildInputs = [ unzip ];
   installPhase = ''
