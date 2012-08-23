bb5080bb7a22694539a52d99aa4cfbeb64c4f51b
Danny Groenewegen <dgroenewegen@gmail.com>
2012-08-23 10:52:00 +0000

    import apache and aselect settings from webdsl server config
    svn path=/; revision=5289

--- webdsldeploy/trunk/configurations/shelley.nix
+++ webdsldeploy/trunk/configurations/shelley.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
+let
+
+  aselect = import ./aselect {
+    inherit (pkgs) stdenv apacheHttpd libtool;
+  };
+
+  feedback = import ./aselect/feedback.nix {
+    inherit (pkgs) stdenv;
+  };
+
+in
+
 {
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
@@ -47,4 +59,105 @@
      + "-XX:+HeapDumpOnOutOfMemoryError -Dorg.apache.tomcat.util.http.ServerCookie.ALLOW_EQUALS_IN_VALUE=true";
   
   deployment.targetHost = "dutieq";
+  
+  jobs.aselect = {
+    name = "aselect";
+
+    startOn = "started httpd";
+
+    preStart = ''
+      if [ ! -d /var/lib/aselect ]
+      then
+          mkdir -p /var/lib/aselect/log/aselectagent/system
+          cp -a ${aselect}/work /var/lib/aselect
+          chmod -R u+rw /var/lib/aselect
+          cp ${feedback}/* /var/lib/aselect/work/aselectagent
+      fi
+    '';
+
+    preStop = "${pkgs.jdk}/bin/java -classpath \"${aselect}/bin/aselectagent/\" StopAgent";
+
+    exec = "${pkgs.jdk}/bin/java -Duser.dir=\"/var/lib/aselect/work/aselectagent\" -server -jar \"${aselect}/bin/aselectag$
+  };
+
+  services.httpd = rec {
+    enable = true;
+
+    logPerVirtualHost = true;
+    logDir = "/data/www/logs";
+    logFormat = "combined";
+
+    extraConfig = ''
+      <Location /server-status>
+        SetHandler server-status
+        Allow from 127.0.0.1 # If using a remote host for monitoring replace 127.0.0.1 with its IP.
+        Order deny,allow
+        Deny from all
+      </Location>
+
+      <IfModule mod_deflate.c>
+        SetOutputFilter DEFLATE
+
+        # Don’t compress
+        SetEnvIfNoCase Request_URI \.(?:gif|jpe?g|png)$ no-gzip dont-vary
+        SetEnvIfNoCase Request_URI \.(?:exe|t?gz|zip|bz2|sit|rar)$ no-gzip dont-vary
+
+        #Dealing with proxy servers
+        <IfModule mod_headers.c>
+            Header append Vary User-Agent
+        </IfModule>
+
+      </IfModule>
+
+      ExtendedStatus On
+
+      <IfModule mod_aselect_filter.c>
+
+      # The location of the error template
+      aselect_filter_set_html_error_template "${aselect}/apachefilter/conf/error_template.html"
+
+      # A-Select Agent IP and port
+      aselect_filter_set_agent_address "127.0.0.1"
+      aselect_filter_set_agent_port "1495"
+
+      # Applications to protect
+      aselect_filter_add_secure_app "/evaluaties" "tudfeedb39de56" "default"
+      aselect_filter_add_secure_app "/weblab/aselectsignin" "tudfeedb39de56" "default"
+      # Global options
+      aselect_filter_set_use_aselect_bar "0"
+      aselect_filter_set_redirect_mode "full"
+
+      # Authorization
+      #aselect_filter_add_authz_rule "app1" "*" "ip=127.0.0.1"
+
+      </IfModule>
+
+    '';
+
+    extraSubservices = [
+      { serviceType = "tomcat-connector";
+        inherit logDir ;
+        stateDir = "/var/run/httpd";
+        extraWorkersProperties = ''
+          worker.list=loadbalancer,loadbalancer2,status
+
+          # modify the host as your host IP or DNS name.
+          worker.node2.port=8010
+          worker.node2.host=localhost
+          worker.node2.type=ajp13
+          worker.node2.lbfactor=1
+
+          # Load-balancing behaviour
+          worker.loadbalancer2.type=lb
+          worker.loadbalancer2.balance_workers=node2
+        '';
+      }
+    ];
+
+    extraModules = [
+      "deflate"
+      { name = "aselect_filter"; path = "${aselect}/modules/mod_aselect_filter.so"; }
+    ];
+  };
+
 }
